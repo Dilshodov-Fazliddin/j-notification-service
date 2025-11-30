d5f38903ed171b1cdab40a76d2007b310636b267
Fazliddin <122451505+Dilshodov-Fazliddin@users.noreply.github.com>
2025-11-30 16:11:54 +0500

    Delete src/main/java/uzumtech/notification/jnotificationservice/repository/NotificationRepository.java

--- src/main/java/uzumtech/notification/jnotificationservice/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package uzumtech.notification.jnotificationservice.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uzumtech.notification.jnotificationservice.model.NotificationEntity;
-
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-}
